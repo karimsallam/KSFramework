dffc3ea3078f9adb84a70c1d956ee46d04d0f42d
Karim Sallam <karim.sallam@me.com>
2012-07-26 12:31:34 +0200

    Added build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+rm -rf build
+rm -rf KSFramework/build
+
+xcodebuild -project KSFramework/KSFramework.xcodeproj -target KSFramework -configuration Release -sdk iphoneos
+xcodebuild -project KSFramework/KSFramework.xcodeproj -target KSFramework -configuration Release -sdk iphonesimulator
+
+mkdir build
+lipo KSFramework/build/Release-iphoneos/libKSFramework.a KSFramework/build/Release-iphonesimulator/libKSFramework.a -create -output build/libKSFramework.a
+
+rm -rf KSFramework/build
+
+mkdir build/Headers
+find KSFramework/KSFramework -type f -name "*.h" -exec cp {} build/Headers \;
+
+mkdir build/Scripts
+find KSFramework/KSFramework -type f -name "*.sh" -exec cp {} build/Scripts \;
+
+lipo -info build/libKSFramework.a
